46a2da28503f8090d2576a08987c55f8c79c2274
Subhankito Roy Choudhury <subhankitoroychoudhuryofficial@gmail.com>
2025-03-10 20:58:36 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById("linkedin").addEventListener("click", function() {
+    window.location.href = "https://www.linkedin.com/in/subhankito-roy-choudhury-96144a314/recent-activity/all/";
+});
+document.getElementById("instagram").addEventListener("click", function() {
+    window.location.href = "https://www.instagram.com/_._.adeyy._/";
+});
+document.getElementById("github").addEventListener("click", function() {
+    window.location.href = "https://github.com/Adey1400";
+});
+document.getElementById("twitter").addEventListener("click", function() {
+    window.location.href = "https://x.com/Subhankito58984";
+});
+document.getElementById("facebook").addEventListener("click", function() {
+    window.location.href = "https://www.facebook.com/subhankito.roychoudhury";
+});
+document.getElementById("discord").addEventListener("click", function() {
+    window.location.href = "https://discord.com/channels/@me";
+});
